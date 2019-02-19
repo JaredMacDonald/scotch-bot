5b72853cf85667d70d02a6ae29a9b5fe35fb2bd0
JaredMacDonald <jared.macdonald@gmail.com>
2019-02-19 09:04:41 -0500

    Adding a .then and .catch to the mongoose.connect call.

--- config/database.js
+++ config/database.js
@@ -5,5 +5,13 @@ exports.mongoDatabaseUrl = process.env.mongoDatabaseUrl;
 let connection;
 
 exports.connectToMongo = () =>  {
-    mongoose.connect(this.mongoDatabaseUrl);
+    //mongoose.connect(this.mongoDatabaseUrl);
+    mongoose.connect(this.mongoDatabaseUrl)
+  .then(() => {
+    //server.start();
+  })
+  .catch((err) => {
+    console.log('Error on start: ' + err.stack);
+    process.exit(1);
+  });
 };
